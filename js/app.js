91b5bf86fdb056a36ba7512bf91f14c0247cfc50
Deceit-87 <manuelfreund318@gmail.com>
2022-02-03 22:46:23 +0100

    codice base funzionante

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 const userName = prompt('come ti chiami bomber ?')
 console.log(userName)
 
+const userSurname = prompt('come ti chiami bomber ?')
+console.log(userSurname)
+
+const userColor = prompt('come ti chiami bomber ?')
+console.log(userColor)
+
+const userAge = prompt('come ti chiami bomber ?')
+console.log(userAge)
+
 const passWord = document.getElementById('user-password')
+
+passWord.innerHTML = userName + userSurname + userColor + userAge
  
